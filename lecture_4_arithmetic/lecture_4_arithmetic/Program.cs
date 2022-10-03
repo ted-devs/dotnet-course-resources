c4050d898d6ed7bf62e4ccd349ab5e7e278e9e79
Talha Salman <talha.salman22@gmail.com>
2022-10-03 21:39:14 +0500

    Updated Resources

---
+++ lecture_4_arithmetic/lecture_4_arithmetic/Program.cs
@@ -0,0 +1,49 @@
+﻿// This line prints out "Hello World!"
+Console.WriteLine("Hello, World!");
+
+// Explicit Typing
+int age;
+age = 32;
+
+// Implicit Typing
+var ageImplicit = 32;
+
+// Constants
+const double pi = 3.14;
+//pi = 3;   This will throw an error. you cannot reassign a const
+
+// Operator Precedence
+
+// Brackets 
+// Order
+// Division, Multiplication
+// Addition, Subtraction
+
+int firstNum = 15;
+int secondNum = 7;
+
+// Addition
+Console.Write("Addition: ");
+Console.WriteLine(firstNum + secondNum);
+
+// Subtraction
+Console.Write("Subtraction: ");
+Console.WriteLine(secondNum - firstNum);
+
+// Multiplication
+Console.Write("Multiplication: ");
+Console.WriteLine(firstNum * secondNum);
+
+// Division
+Console.Write("Division: ");
+Console.WriteLine(firstNum / secondNum);
+
+Console.Write("Division: ");
+Console.WriteLine(15 / 6.0);
+
+//Console.Write("Division: ");
+//Console.WriteLine(firstNum / 0);
+
+// Modulus
+Console.Write("Modulus: ");
+Console.WriteLine(firstNum % secondNum);
