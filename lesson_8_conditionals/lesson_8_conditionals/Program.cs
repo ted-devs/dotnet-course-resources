0d0bf5622989a60e56febbcb72de677e0578b2fb
Talha Salman <talha.salman22@gmail.com>
2022-10-22 16:18:39 +0500

    Lesson 9 Added

---
+++ lesson_8_conditionals/lesson_8_conditionals/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Write("Enter your Name: ");
+string name = Console.ReadLine();
+
+if (name == "Talha")
+    Console.WriteLine("Welcome Teacher!");
+else if (name == "Amad")
+    Console.WriteLine("Welcome Student!");
+else
+    Console.WriteLine("You are not welcome here.");
+
+Console.WriteLine("Outside if block.");
