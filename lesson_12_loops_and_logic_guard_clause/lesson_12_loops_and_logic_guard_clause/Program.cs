3cb5ba90fb67dfd42a767e81aeb8fcb0aaa26a84
Talha Salman <talha.salman22@gmail.com>
2022-10-30 17:28:48 +0500

    lesson 12

---
+++ lesson_12_loops_and_logic_guard_clause/lesson_12_loops_and_logic_guard_clause/Program.cs
@@ -0,0 +1,30 @@
+﻿// Guard Clause technique
+
+int age = 20;
+double height = 6;
+double weight = 65;
+
+if (age < 18) return;
+if (age > 30) return;
+if (height < 5.5) return;
+if (weight < 60) return;
+if (weight > 80) return;
+
+Console.WriteLine("You pass");
+
+//if (age > 18)
+//{
+//    if (age < 30)
+//    {
+//        if (height > 5.5)
+//        {
+//            if (weight > 60)
+//            {
+//                if (weight < 80)
+//                {
+//                    Console.WriteLine("You pass");
+//                }
+//            }
+//        }
+//    }
+//}
