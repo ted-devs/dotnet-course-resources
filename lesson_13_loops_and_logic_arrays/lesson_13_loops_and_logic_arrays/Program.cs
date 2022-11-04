3290b6d41060ae27744c6571b8ec6b477558408b
Talha Salman <talha.salman22@gmail.com>
2022-11-05 00:36:20 +0500

    code lesson13

--- lesson_13_loops_and_logic_arrays/lesson_13_loops_and_logic_arrays/Program.cs
+++ lesson_13_loops_and_logic_arrays/lesson_13_loops_and_logic_arrays/Program.cs
@@ -1,9 +1,22 @@
-﻿int[] numbers = { 1, 2, 3, 4 };
+﻿
+// Example with Array
+int[] numbers = { 1, 4, 3, 2 };
 
 int i = 0;
 while (i < numbers.Length)
-{
     Console.WriteLine(numbers[i++]);
-}
 
 Console.WriteLine($"Sum: {numbers.Sum()}");
+
+// Example with variables
+int num1 = 1;
+int num2 = 4;
+int num3 = 3;
+int num4 = 2;
+
+Console.WriteLine(num1);
+Console.WriteLine(num2);
+Console.WriteLine(num3);
+Console.WriteLine(num4);
+
+int sum = num1 + num2 + num3 + num4;
